c64fdd73ecb60c878a66f484a4db67faaeb65b5b
Teta <example@example.com>
2024-09-30 12:17:35 +0500

    Added local "db" and comments

--- Aduplicate/Consumer.py
+++ Aduplicate/Consumer.py
@@ -3,19 +3,28 @@ from kafka.admin import KafkaAdminClient, NewTopic
 import json
 
 class Consumer(KafkaConsumer):
+    '''
+    Данный класс это надстройка над классом KafkaConsumer
+    Нужен этот класс для гарантии того что одно сообщение обработается ровно 1 раз
+    '''
     def __init__(self, **configs):
         super().__init__(**configs)
         # Сохраняем аргументы в атрибутах экземпляра
         for key, value in configs.items():
             setattr(self, key, value)
-        self._create_special_topic()
-        self._special_message = self._get_messages_special_topic()
+        self._create_special_topic() # Проверяем создание и создаём спец. тему
+        
+        # Создаём удобный словарь обработанных сообщений
+        dict_of_verified_messages = self._local_db_of_checked_messages_hashes()
 
     def get_config(self, key):
-        """Метод для получения значения конфигурации по ключу."""
+        '''Метод для получения значения конфигурации по ключу'''
         return getattr(self, key, None)
 
     def _create_special_topic(self) -> None:
+        '''Здесь проверяется создание специальной темы, для хранения всех
+        обработанных сообщений'''
+        
         # Создаем экземпляр KafkaAdminClient
         admin_client = KafkaAdminClient(bootstrap_servers=self.get_config("bootstrap_servers"))
 
@@ -29,6 +38,9 @@ class Consumer(KafkaConsumer):
             admin_client.create_topics(new_topics=[new_topic], validate_only=False)
     
     def _get_messages_special_topic(self) -> list:
+        '''Здесь мы получаем список всех 
+        проверенных/обработанных сообщений из всех тем и партиций'''
+        
         # Создаем потребителя
         consumer = KafkaConsumer(
             bootstrap_servers=self.get_config("bootstrap_servers"),
@@ -56,3 +68,28 @@ class Consumer(KafkaConsumer):
 
         return messages
 
+    def _local_db_of_checked_messages_hashes(self) -> dict:
+        '''Данная функция создаёт удобный словарь с хэшами проверенных сообщений'''
+        
+        # Получение списка проверенных сообщений
+        special_messages = self._get_messages_special_topic()
+        
+        # Проверка на наличие сообщений
+        if special_messages != []:
+            
+            # Создаем словарь для хранения списков по partition
+            partitioned_data = {}
+
+            # Разделяем данные по partition
+            for item in special_messages:
+                partition = item["partition"]
+                if partition not in partitioned_data:
+                    partitioned_data[partition] = []
+                partitioned_data[partition].append(item["hash"])
+
+            return partitioned_data
+    
+    
+    
+    
+    
